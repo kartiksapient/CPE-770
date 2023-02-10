661bb6ac00aa8bfc6139a3d2f022c356567a1950
Kartik Bansal <kartik.bansal@publicissapient.com>
2023-02-10 21:34:44 +0530

    new

--- fwr/main.tf
+++ fwr/main.tf
@@ -3,5 +3,5 @@ resource "google_compute_subnetwork" "default" {
   name          = var.name2
   ip_cidr_range = "10.0.1.0/24"
   region        = var.region
-  network       = google_compute_network.vpc_network.id
+  network       = "default"
 }
