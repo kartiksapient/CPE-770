c7360429f8f748d7990ef5dd0e0c9282a7c60297
Kartik Bansal <kartik.bansal@publicissapient.com>
2023-02-10 21:40:36 +0530

    new

--- DEV/variables.tf
+++ DEV/variables.tf
@@ -1,12 +1,12 @@
 variable "name"{
     type = string
-    
+    default = "dev"
 }
 variable "name2"{
     type = string
-
+    default = "dev2"
 }
 variable "region"{
     type = string
- 
+    default = "us-west2"
 }
