bcbf068601c0d6bf71f8439c36a6a1d36db5b294
Kartik Bansal <kartik.bansal@publicissapient.com>
2023-02-10 20:27:10 +0530

    12

--- DEV/main.tf
+++ DEV/main.tf
@@ -1,11 +1,10 @@
 module "vpc_network" {
     source        = "../IAM"
-    region        = "us-west1"
+    
 
 }
 
 module "default" {
     source        = "../fwr"
-    name          = "dev2"
-    region        = "us-west1"
+    
 }
