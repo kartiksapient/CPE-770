67231ea41d16f0ae955d67e7fa620d91ea0ae495
Kartik Bansal <kartik.bansal@publicissapient.com>
2023-02-10 19:15:54 +0530

    12

--- fwr/main.tf
+++ fwr/main.tf
@@ -1,7 +1,7 @@
 
 resource "google_compute_subnetwork" "default" {
 
-  name          = "my-custom-subnet"
+  name          = var.name2
   ip_cidr_range = "10.0.1.0/24"
   region        = var.region
   network       = google_compute_network.vpc_network.id
