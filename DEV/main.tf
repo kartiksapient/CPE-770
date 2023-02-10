c201b9eaaafd1fdb465d5fbd762418db3715bac7
Kartik Bansal <kartik.bansal@publicissapient.com>
2023-02-10 19:07:50 +0530

    2

--- DEV/main.tf
+++ DEV/main.tf
@@ -1,14 +1,7 @@
 module "vpc_network" {
   source                  = "../IAM"
-  name                    = var.name
-  auto_create_subnetworks = false
-  mtu                     = 1460
 }
 
 module "default" {
   source                  = "../fwr"
-  name          = "my-custom-subnet"
-  ip_cidr_range = "10.0.1.0/24"
-  region        = var.region
-  network       = google_compute_network.vpc_network.id
 }
