f062093a80c980a4391c49fd5c55481fea71d1ff
Kartik Bansal <kartik.bansal@publicissapient.com>
2023-02-14 17:06:56 +0530

    vpc

--- Vpc/veriable.tf
+++ Vpc/veriable.tf
@@ -2,3 +2,14 @@ variable "name"{
     type = string
   
 }
+
+variable "auto_create_subnetworks" {
+    type = bool
+    # default = false
+  
+}
+variable "mtu" {
+    type = string
+    # default = "1460"
+  
+}
