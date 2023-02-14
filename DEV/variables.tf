dc69c7634900185165eb7fd0a426eb6057fa881f
Kartik Bansal <kartik.bansal@publicissapient.com>
2023-02-14 16:13:56 +0530

    dev

--- DEV/variables.tf
+++ DEV/variables.tf
@@ -31,9 +31,9 @@ variable "region"{
     type = string
     default = "us-west2"
 }
-# variable "dev_subnet_address" {
-#     type = string
-#     default = "10.0.1.1/24"
-# #   type = list(string)
-# #   default = [ "10.0.1.0/24" ]
-# }
+variable "dev_subnet_address" {
+    type = string
+    default = "10.0.1.1/24"
+#   type = list(string)
+#   default = [ "10.0.1.0/24" ]
+}
