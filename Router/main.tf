58b82568f4043d76aa554d65bf06e16d8da7b9ed
Kartik Bansal <kartik.bansal@publicissapient.com>
2023-02-14 16:19:05 +0530

    new

--- Router/main.tf
+++ Router/main.tf
@@ -2,7 +2,7 @@ resource "google_compute_router" "router" {
   name    = var.name
   region  = var.region
   network = google_compute_network.network.self_link 
-  # network = var.net-name
+  network = var.net-name
 
 
   bgp {
