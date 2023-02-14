11fc9031d9f8944995ca9eb43e0d58b7ad48319b
Kartik Bansal <kartik.bansal@publicissapient.com>
2023-02-14 16:25:58 +0530

    nwe

--- Subnetwork/main.tf
+++ Subnetwork/main.tf
@@ -2,7 +2,7 @@
 resource "google_compute_subnetwork" "default" {
 
   name          = var.name
-  ip_cidr_range = var.subnet-address
+  subnet-address = var.subnet-address
   region        = var.region
   network       = "default"
   # network = google_compute_network.network.self_link
