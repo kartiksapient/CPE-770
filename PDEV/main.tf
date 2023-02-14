b1341d4e211e0cf85a72f50d3f0dacb93bcd91d7
Kartik Bansal <kartik.bansal@publicissapient.com>
2023-02-14 16:51:48 +0530

    pdev

--- PDEV/main.tf
+++ PDEV/main.tf
@@ -1,3 +1,19 @@
+# module "vpc_network" {
+#     source        = "../Vpc"
+#     name = var.vpc_name
+# }
+
+# module "default" {
+
+#   source        = "../Subnetwork"
+#   name          = var.subnetwork_name
+#   region        = var.region
+# }
+module "dev_network" {
+    source        = "../Net"
+    name = var.pdev_net_name
+}
+
 module "vpc_network" {
     source        = "../Vpc"
     name = var.vpc_name
@@ -7,5 +23,23 @@ module "default" {
 
   source        = "../Subnetwork"
   name          = var.subnetwork_name
+  subnet_address = var.pdev_subnet_address
+  net_name = var.pdev_net_name
   region        = var.region
+}
+
+module "router" {
+    source        = "../Router"
+    name = var.router_name
+    region        = var.region
+    net-name = var.pdev_net_name
+}
+module "nat" {
+
+  source        = "../Nat"
+  name          = var.nat_name
+#   nat_ip_allocate_option = var.allocate_option
+#   ip_cidr_range = var.dev_subnet_address
+  region        = var.region
+  router        = var.router_name
 }
--- PDEV/variables.tf
+++ PDEV/variables.tf
@@ -1,3 +1,36 @@
+# variable "vpc_name"{
+#     type = string
+#     default = "pdev-vpc"
+# }
+# variable "subnetwork_name"{
+#     type = string
+#     default = "pdev-subnetwork"
+# }
+# variable "region"{
+#     type = string
+#     default = "us-west2"
+# }
+variable "pdev_net_name"{
+    type = string
+    default = "dev-net"
+}
+
+variable "nat_name"{
+    type = string
+    default = "pdev-nat"
+}
+
+variable "router_name"{
+    type = string
+    default = "pdev-router"
+    # network = var.dev_net_name
+}
+
+
+variable "allocate_option"{
+    type = string
+    default = "AUTO_ONLY"
+}
 variable "vpc_name"{
     type = string
     default = "pdev-vpc"
@@ -9,4 +42,8 @@ variable "subnetwork_name"{
 variable "region"{
     type = string
     default = "us-west2"
+}
+variable "pdev_subnet_address" {
+    type = string
+    default = "10.0.1.1/24"
 }
